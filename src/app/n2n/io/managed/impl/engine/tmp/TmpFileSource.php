7bce31e6ee4ed7a767d608e87ba3bf736a068042
Andreas von Burg <a@von-burg.net>
2022-08-30 13:06:16 +0200

    php 8.1 compatibility

--- src/app/n2n/io/managed/impl/engine/tmp/TmpFileSource.php
+++ src/app/n2n/io/managed/impl/engine/tmp/TmpFileSource.php
@@ -41,12 +41,12 @@ class TmpFileSource extends FileSourceAdapter {
 		return $this->sessionId;
 	}
 	
-	public function __serialize() {
+	public function __serialize(): array {
 		return array('qualifiedName' => $this->qualifiedName, 'fileFsPath' => $this->fileFsPath,
 				'fileManagerName' => $this->fileManagerName, 'url' => $this->url, 'sessionId' => $this->sessionId);
 	}
 	
-	public function __unserialize(array $data) {
+	public function __unserialize(array $data): void {
 		UnserializationFailedException::assertTrue(isset($data['qualifiedName']) && ($data['qualifiedName'] === null || is_string($data['qualifiedName']))
 				&& isset($data['fileFsPath']) && $data['fileFsPath'] instanceof FsPath
 				&& array_key_exists('fileManagerName', $data) && is_string($data['fileManagerName'])
